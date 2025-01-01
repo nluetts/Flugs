3b1d64e0d6d5879b75287a9ea888f79d04565455
Nils Luettschwager <nluetts@gwdg.de>
2025-01-02 00:13:51 +0100

    WIP send channels via trait

--- src/app.rs
+++ src/app.rs
@@ -1,65 +1,123 @@
+use log::warn;
+use std::sync::mpsc::{Receiver, Sender, TryRecvError};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+pub struct App {
+    counter: UIParameter<i64>,
+    request_tx: Sender<Box<dyn BackendRequest>>,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
+struct UIParameter<T> {
+    pending_update_rx: Option<Receiver<T>>,
+    value: T,
+}
+
+/// The linker is send to the backend thread and replies
+/// once the action ran on the backend.
+struct Linker<T, F>
+where
+    F: Fn(&mut Backend) -> T,
+{
+    backchannel: Sender<T>,
+    action: F,
+}
+
+impl<T, F> Linker<T, F>
+where
+    F: Fn(&mut Backend) -> T,
+{
+    fn new(backchannel: Sender<T>, action: F) -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            backchannel,
+            action,
         }
     }
 }
 
-impl TemplateApp {
+trait BackendRequest {
+    fn run_on_backend(self, backend: &mut Backend);
+}
+
+impl<T, F> BackendRequest for Linker<T, F>
+where
+    F: Fn(&mut Backend) -> T,
+{
+    fn run_on_backend(self, backend: &mut Backend) {
+        let result = (self.action)(backend);
+        self.backchannel
+            .send(result)
+            .expect("The Backchannel is open to receive answers.")
+    }
+}
+
+pub struct Backend {
+    value: i64,
+}
+
+impl Backend {
+    fn increase_counter(&mut self) {
+        self.value += 1;
+    }
+    fn decrease_counter(&mut self) {
+        self.value -= 1;
+    }
+}
+
+impl<T> UIParameter<T> {
+    fn new(val: T) -> Self {
+        UIParameter {
+            pending_update_rx: None,
+            value: val,
+        }
+    }
+}
+
+impl App {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+    pub fn new(
+        cc: &eframe::CreationContext<'_>,
+        request_tx: Sender<Box<dyn BackendRequest>>,
+    ) -> Self {
+        Self {
+            counter: UIParameter::new(0),
+            request_tx,
         }
+    }
 
-        Default::default()
+    fn try_update_counter(&mut self) -> bool {
+        let ui_enabled;
+        match &self.counter.pending_update_rx {
+            Some(rx) => match rx.try_recv() {
+                Ok(val) => {
+                    self.counter.value = val;
+                    self.counter.pending_update_rx = None;
+                    ui_enabled = true
+                }
+                Err(err) => match err {
+                    TryRecvError::Empty => ui_enabled = false,
+                    TryRecvError::Disconnected => {
+                        warn!("Tried to receive message from closed channel.");
+                        self.counter.pending_update_rx = None;
+                        ui_enabled = true;
+                    }
+                },
+            },
+            None => ui_enabled = true,
+        }
+        ui_enabled
     }
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
+impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+                ui.add_space(16.0);
 
                 egui::widgets::global_theme_preference_buttons(ui);
             });
@@ -67,43 +125,36 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.heading("overengineered counter");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            let ui_enabled = self.try_update_counter();
+            let increment_button = egui::Button::new("Increment");
+            let decrement_button = egui::Button::new("Decrement");
             if ui.button("Increment").clicked() {
-                self.value += 1.0;
+                let (tx, rx) = std::sync::mpsc::channel();
+                self.counter.pending_update_rx = Some(rx);
+                let linker = Linker::new(tx, |b| {
+                    b.increase_counter();
+                    b.value
+                });
+                self.request_tx
+                    .send(Box::new(linker))
+                    .expect("Trying to send value via closed channel.");
             }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            if ui.button("Decrement").clicked() {
+                let (tx, rx) = std::sync::mpsc::channel();
+                self.counter.pending_update_rx = Some(rx);
+                let linker = Linker::new(tx, |b| {
+                    b.decrease_counter();
+                    b.value
+                });
+                self.request_tx
+                    .send(Box::new(linker))
+                    .expect("Trying to send value via closed channel.");
+            }
+            ui.add_enabled(ui_enabled, increment_button);
+            ui.label(format!("counter {}", self.counter.value));
+            ui.add_enabled(ui_enabled, decrement_button);
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
